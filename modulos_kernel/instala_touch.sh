b54dbf2f5c1c420125fd0480e9c019e9d7ac88d5
juliovendramini <juliovendramini@users.noreply.github.com>
2020-01-03 02:03:55 -0300

    modulo bt

--- modulos_kernel/instala_touch.sh
+++ modulos_kernel/instala_touch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MODULE_NAME="gslx680_ts_acpi"
 KVER=$(uname -r)
-MODDESTDIR="/lib/modules/$(KVER)/kernel/drivers/input/touchscreen"
+MODDESTDIR="/lib/modules/$KVER/kernel/drivers/input/touchscreen"
 install -p -m 644 "$MODULE_NAME.ko"  "$MODDESTDIR"
 /sbin/depmod -a $KVER
